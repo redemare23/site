f508ae5013f5473336ad837adc4532fd98c0c313
redemare <redemare23@gmail.com>
2024-03-17 11:01:15 -0300

    teste fazer aparecer caixa no celular

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    window.addEventListener("scroll", function() {
+  window.addEventListener("scroll", function() {
       let caixas = document.querySelectorAll('.caixa');
       for (let caixa of caixas) {
-        if (isElementInViewport(caixa)) {
-          caixa.classList.add('aparecer');
-        }
+          if (isElementInViewport(caixa)) {
+              caixa.classList.add('aparecer');
+          }
       }
-    });
-  
-    function isElementInViewport(el) {
-      let rect = el.getBoundingClientRect();
-      return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-      );
-    }
   });
-  
+
+  function isElementInViewport(el) {
+      let rect = el.getBoundingClientRect();
+      // Verifica se o elemento está visível na viewport, considerando uma margem
+      return (
+          rect.top >= -50 &&
+          rect.left >= 0 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + 50 &&
+          rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+  }
+});
