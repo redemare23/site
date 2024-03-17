8df1bbab3e1c1df7d4178067395ee8e5ac9c0258
redemare <redemare23@gmail.com>
2024-03-17 11:07:12 -0300

    teste animação no celular

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   window.addEventListener("scroll", function() {
       let caixas = document.querySelectorAll('.caixa');
       for (let caixa of caixas) {
-          if (isElementInViewport(caixa)) {
+          if (isElementInViewport(caixa) || isMobile()) {
               caixa.classList.add('aparecer');
           }
       }
@@ -10,12 +10,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
   function isElementInViewport(el) {
       let rect = el.getBoundingClientRect();
-      // Verifica se o elemento está visível na viewport, considerando uma margem
       return (
-          rect.top >= -50 &&
+          rect.top >= 0 &&
           rect.left >= 0 &&
-          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + 50 &&
+          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
           rect.right <= (window.innerWidth || document.documentElement.clientWidth)
       );
+  }
+
+  function isMobile() {
+      return window.innerWidth <= 767; // Defina o limite de largura que considera como dispositivo móvel
   }
 });
